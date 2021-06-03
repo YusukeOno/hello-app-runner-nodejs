86db2a00d7ee0ee30ebfb62ee85f48c6a575e151
小野裕輔 <y-ono@nswt.co.jp>
2021-06-03 16:36:02 +0900

    Update something

--- components/layout.js
+++ components/layout.js
@@ -7,7 +7,7 @@ import Link from "next/link";
 import Logo from "./logo";
 
 export const siteTitle = "Hello App Runner and Copilot!";
-export const topPageMessage = "Happy building with App Runner and Copilot! (By YusukeOno)";
+export const topPageMessage = "Happy building with App Runner and Copilot!";
 
 export default function Layout({ children, home }) {
   return (
